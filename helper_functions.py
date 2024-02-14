0119a27dff3b8c233a0073a16fa3709776a629c1
Parth Shah <parth@parths-mbp.dhcp.asu.edu>
2024-02-14 14:23:38 -0700

    restarting

--- helper_functions.py
+++
@@ -1,7 +0,0 @@
-from bsedata.bse import BSE
-
-
-def get_current_price(company_id):
-    b = BSE()
-    q = b.getQuote(company_id)
-    return (float(q["currentValue"]))
--- test.py
+++
@@ -1,6 +0,0 @@
-from bsedata.bse import BSE
-
-b = BSE()
-q = b.getQuote("534976")
-
-print(type(q["currentValue"]))
